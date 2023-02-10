3ca5da7a7830d9912946325f95e678ddd183990a
SageKyle <ominyipaulo@gmail.com>
2023-02-10 23:03:50 +0100

    chore: remove toast from useDocument

--- src/hooks/useDocument.jsx
+++ src/hooks/useDocument.jsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from 'react';
-import toast from 'react-hot-toast';
 import { projectFirestore } from '../firebase/config';
 
 export const useDocument = (collection, id) => {
